8c0ea74f482cf2664af710e94b8913024b1fd2d2
dedi19 <64629058+dedi19@users.noreply.github.com>
2020-05-05 16:33:46 +0700

    Add files via upload

---
+++ 1-4-2020 Variable.ipynb.py
@@ -0,0 +1,11 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> " SCHOOL SCHEDULE SUBJECTS"
+' SCHOOL SCHEDULE SUBJECTS'
+>>> Monday="Science_Math_Chemistry"
+>>> Tuesday=" English_Social study_History"
+>>> Wednesday="Gym_swimming_Music"
+>>> Thursday="drawing_Math_sports"
+>>> Friday="English_Math_science"
+>>> Saturday="Scout_activity_School_cleaning"
+>>> 
