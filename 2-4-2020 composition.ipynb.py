d0c28746e6a9b4d8e34b829041289ca523561655
dedi19 <64629058+dedi19@users.noreply.github.com>
2020-05-05 16:29:11 +0700

    Add files via upload

---
+++ 2-4-2020 composition.ipynb.py
@@ -0,0 +1,10 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> " ENGLISH COMPOSITION 1  "
+' ENGLISH COMPOSITION 1  '
+>>> Tittle=" CARONA VIRUS"
+>>> WRITER="Dedi Erianto Manullang"
+>>> " Carona virus is one of a dangerous virus. This virus was first time from  Wuhan China and spread to all over the world. This virus already Killed thousand of People in China and spread to other countries such as: Russia, Indonesia, Malaysia, Singapore, India, America, Italy. Scientist unable to discover this anti vaccine for this Virus that's why this virus to be mysterious kill people everyday In Italy one of a biggest victims the deat toll more than ten thousand people. Many people lost their life, lost their Job, Bankcrupt, starving, ploundering. we Hope for God so this virus can end up soon. No one can predict what is going on just let's pray to the God so this Virus doesn't spread again"
+" Carona virus is one of a dangerous virus. This virus was first time from  Wuhan China and spread to all over the world. This virus already Killed thousand of People in China and spread to other countries such as: Russia, Indonesia, Malaysia, Singapore, India, America, Italy. Scientist unable to discover this anti vaccine for this Virus that's why this virus to be mysterious kill people everyday In Italy one of a biggest victims the deat toll more than ten thousand people. Many people lost their life, lost their Job, Bankcrupt, starving, ploundering. we Hope for God so this virus can end up soon. No one can predict what is going on just let's pray to the God so this Virus doesn't spread again"
+>>> 
+>>> 
