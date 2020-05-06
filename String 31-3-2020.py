7933fb06916713205c6a7d56315022726e6e0de3
dedi19 <64629058+dedi19@users.noreply.github.com>
2020-05-06 21:41:56 +0700

    Add files via upload

---
+++ String 31-3-2020.py
@@ -0,0 +1,22 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Dedi Manullang Pet shop")
+Dedi Manullang Pet shop
+>>> kucing=200000
+>>> anjing=500000
+>>> ayam=70000
+>>> kelinci=50000
+>>> print("harga kucing+anjing+ayam+kelinci")
+harga kucing+anjing+ayam+kelinci
+
+>>> kucing
+200000
+>>> anjing
+500000
+>>> ayam
+70000
+>>> ayam+anjing
+570000
+>>> ayam-anjing
+-430000
+>>> 
